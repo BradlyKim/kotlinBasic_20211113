4605588aece926fcd2197c9f74113760fd2ea27e
김정환 <kjh5864@naver.com>
2021-11-13 14:01:10 +0900

    한 번 저장한 변수는, 여러번 활용해도 됨.  -ex. 토스트 띄우는 문구로 활용

--- app/src/main/java/com/nepplus/kotlinbasic_20211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20211113/MainActivity.kt
@@ -41,6 +41,10 @@ class MainActivity : AppCompatActivity() {
 
             txtContent.text = inputContent
 
+//          3. 입력한 내용을 토슽로 출력
+
+            Toast.makeText(this, inputContent, Toast.LENGTH_SHORT).show()
+
 
         }
 
